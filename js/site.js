8c06df1f46c74a9f9783db3bb5e6a7973e691a9e
kpuiiuiii <mmelnyk@hawk.iit.edu>
2021-03-16 17:37:18 -0500

    8th commit
    Add image slider for content

--- js/site.js
+++ js/site.js
@@ -1,11 +1,18 @@
 /*JavaScript added on this page */
+var slides = document.querySelectorAll("#slides .slide");
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
 
+function nextSlide() {
+  slides[currentSlide].className = "slide";
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = "slide showing";
+}
 
-  function clean() {
-
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("birthday").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("name").focus();
-  }
+function clean() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("birthday").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("name").focus();
+}
