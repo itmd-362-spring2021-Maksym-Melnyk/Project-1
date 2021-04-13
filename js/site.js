185c168c73cd1255db08111a802e48ba7f8d52fd
Maksym Melnyk <43916250+kpuiiuiii@users.noreply.github.com>
2021-04-12 23:55:42 -0500

    Final project turn in
    Add a DOM loader to js file
    Add a query that shrinks the image in css
    Change location of the script tag and add async to it

--- js/site.js
+++ js/site.js
@@ -1,26 +1,28 @@
 // JavaScript code will be here
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
 
+  var slides = document.querySelectorAll("#slides .slide");
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide, 6000);
 
-var slides = document.querySelectorAll("#slides .slide");
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 6000);
+  function nextSlide() {
+    slides[currentSlide].className = "slide";
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].className = "slide showing";
+  }
 
-function nextSlide() {
-  slides[currentSlide].className = "slide";
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].className = "slide showing";
-}
+  function clean() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("birthday").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("name").focus();
+  }
 
-function clean() {
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("birthday").value = "";
-  document.getElementById("phone").value = "";
-  document.getElementById("name").focus();
-}
-
-function thanks() {
-  window.alert("Thank you!" + "\n" + "Welcome to the community!");
-      clean();
-}
+  function thanks() {
+    window.alert("Thank you!" + "\n" + "Welcome to the community!");
+    clean();
+  }
+})
